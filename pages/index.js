9466df5a298d108f6fd5c60a14df6a2cead8cbab
Joseph Rubino <joeyrubi22@gmail.com>
2022-07-28 11:24:12 -0500

    updated index.js for this app

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,27 @@
+import "./styles.css";
 
-
-const Index = ({warranties}) => {
-
-    const warrantyButton = async (_id) => {
-
-        const requestOptions = {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-        };
-        const response = await fetch(`https://v-syndicate-warranty-app.herokuapp.com/warranty/${_id}`, requestOptions);
-        await response.json();   
-        location.reload()
-        return false
-    }
-
+const Index = ({member}) => {
 
     return(
-   warranties.map(({_id, customerEmail, product, purchaseLocation, warrantyStatus}) => (
-        <span>
-        <p key={_id}><span>Warranty Id: {_id.slice(_id.length - 6)} </span>|<span> Customer Email: {customerEmail} </span>
-         |<span> Product: {product} </span> |<span> Purchase Location: {purchaseLocation} </span>
-         |<span key={_id}> Warranty Status: {warrantyStatus} </span>         
-         {warrantyStatus === "active" &&
-          <button id="wb" onClick={() => warrantyButton(_id)} key={_id}>Warranty Claimed</button>
-         }</p> 
+   member.map(({customerEmail, cardTier, accountNumber}) => (
+        <span border>
+        <p>  
+            <span> Customer Email: {customerEmail} </span>
+            |<span> Membership Tier: {cardTier} </span>
+            |<span> Account Number: {accountNumber} </span>         
+            <button id="wb">Make Edit</button>
+        </p> 
         </span>
     )))
 }
 
 export async function getServerSideProps(){
-    const response = await fetch('https://v-syndicate-warranty-app.herokuapp.com/warranties')
+    const response = await fetch('http://localhost:5000/members')
     const data = await response.json()
 
     return {
         props: {
-            warranties: data
+            member: data
         }
     }
 }
