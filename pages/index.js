fc0d39e16000e55a976c7d9e951de355be641c16
Joseph Rubino <joeyrubi22@gmail.com>
2023-01-31 11:33:12 -0600

    enu

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,67 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
-const Index = ({member}) => {
+const Index = ({ member }) => {
+  // function click() {
+  //     const router = useRouter()
 
-// function click() {
-//     const router = useRouter()
+  //     router.push(
+  //         {pathname: `/view-member/${member.accountNumber}`, query: {accountNumber: member.accountNumber}}, `/view-member/${member.accountNumber}`
+  //     );
+  // }
+  return (
+    <>
+      <div>
+        <ul>
+          <li>
+            <Link href="/admin/dashboard">
+              <a className="font-bold">Dashboard</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/admin/orders">Orders</Link>
+          </li>
+          <li>
+            <Link href="/admin/products">Products</Link>
+          </li>
+          <li>
+            <Link href="/admin/users">Users</Link>
+          </li>
+          <li>
+            <Link href="/admin/media">Media</Link>
+          </li>
+        </ul>
+      </div>
+      {member.map((_Id, customerEmail, cardTier, accountNumber) => (
+      <div key={_Id} className="border">
+        <p>
+          <span> Customer Email: {customerEmail} </span>|
+          <span> Membership Tier: {cardTier} </span>|
+          <span> Account Number: {accountNumber} </span>
+          <Link
+            key={_Id}
+            href={`/view-member/${accountNumber}`}
+            className="editButton"
+          >
+            <button className="editButton">View Member</button>
+          </Link>
+        </p>
+      </div>
+      ))}
+    </>
+  );
+};
 
-//     router.push(
-//         {pathname: `/view-member/${member.accountNumber}`, query: {accountNumber: member.accountNumber}}, `/view-member/${member.accountNumber}`
-//     );
-// }
-    return(
-   member.map(({_Id, customerEmail, cardTier, accountNumber}) => (
-        <div key={_Id} className="border">
-        <p>  
-            <span key={customerEmail}> Customer Email: {customerEmail} </span>
-            |<span key={cardTier}> Membership Tier: {cardTier} </span>
-            |<span key={accountNumber}> Account Number: {accountNumber} </span>         
-            <Link key={_Id} href={`/view-member/${accountNumber}`} className="editButton">
-                <button className="editButton">View Member</button>
-                </Link>
-        </p> 
-        </div>
-    )))
+export async function getServerSideProps() {
+  const response = await fetch(
+    'https://syndicate-member.herokuapp.com/api/members'
+  );
+  const data = await response.json();
+
+  return {
+    props: {
+      member: data,
+    },
+  };
 }
 
-export async function getServerSideProps(){
-    const response = await fetch('https://syndicate-member.herokuapp.com/api/members')
-    const data = await response.json()
-
-    return {
-        props: {
-            member: data
-        }
-    }
-}
-
-
-export default Index
+export default Index;
