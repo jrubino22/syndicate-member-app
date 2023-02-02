4706a768079ac27e2795d95f1a2d619a31a121a5
Joseph Rubino <joeyrubi22@gmail.com>
2023-02-02 17:53:26 -0600

    adf

--- server.js
+++ server.js
@@ -306,7 +306,7 @@ app.prepare().then(() => {
   }
   keepAwake('https://syndicate-member.herokuapp.com');
 
-  router.get('(.*)', handleRequest);
+  router.get('(.*)', verifyRequest);
   server.use(router.allowedMethods());
   server.use(router.routes());
 
