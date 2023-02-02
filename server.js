1721982ab8a1679894831d2468d001b34f17eb2a
Joseph Rubino <joeyrubi22@gmail.com>
2023-02-02 17:45:23 -0600

    fda

--- server.js
+++ server.js
@@ -13,16 +13,13 @@ const bodyParser = require('koa-body');
 const cors = require('@koa/cors');
 const google_cal = require('./google_calendar');
 const shopifyApiCalls = require('./shopifyApiCalls');
-import {createShopifyAuth, verifyRequest} from "simple-koa-shopify-auth"
+import { createShopifyAuth, verifyRequest } from 'simple-koa-shopify-auth';
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, () => {
   console.log('Connected to Mongo DB');
 });
-
-const verifyApiRequest = verifyRequest({ returnHeader: true });
-const verifyPageRequest = verifyRequest();
 
 Shopify.Context.initialize({
   API_KEY: process.env.SHOPIFY_API_KEY,
@@ -49,263 +46,257 @@ app.prepare().then(() => {
   server.keys = [Shopify.Context.API_SECRET_KEY];
 
   router.get('/install', async (ctx) => {
-  server.use(
-    createShopifyAuth({
-      accessMode: "offline",
-      authPath: "/install/auth",
-      async afterAuth(ctx) {
-        const { shop, accessToken } = ctx.state.shopify;
-        const { host } = ctx.query;
-        if (!accessToken) {
-          // This can happen if the browser interferes with the auth flow
-          ctx.response.status = 500;
-          ctx.response.body = "Failed to get access token! Please try again.";
-          return;
-        }
-        // Redirect to user auth endpoint, to get user's online token
-        ctx.redirect(`/auth?shop=${shop}&host=${host}`);
-      },
-    })
-  );
-})
-
-server.use(
-  createShopifyAuth({
-    accessMode: "online",
-    authPath: "/auth",
-    async afterAuth(ctx) {
-      const { shop } = ctx.state.shopify;
-      const { host } = ctx.query;
-      // Check if the app is installed
-      // NOTE: You can replace with your own function to check if the shop is installed, or you can just remove it, but this is an extra check that can help prevent auth issues
-      if (isShopActive(shop)) {
-        // Redirect to app
-        ctx.redirect(`/?shop=${shop}&host=${host}`);
-      } else {
-        // Redirect to installation endpoint to get permanent access token
-        ctx.redirect(`/install/auth/?shop=${shop}&host=${host}`);
-      }
-    },
-  })
-);
-
-
-
-  router.get('(.*)', verifyRequest(), async (ctx) => {
-    // Your application code goes here
-  
-
-  router.get('(/_next/static/.*)', verifyRequest);
-  router.get('/_next/webpack-hmr', verifyRequest);
-
-
-  //get all unregistered cards
-  router.get('/api/unregistered', async (ctx) => {
-    ctx.body = await unregisteredIdModel.find();
-  });
-
-
-  //get one unregistered card by account number
-  router.get('/api/unregistered/:memberNum', async (ctx) => {
-    try {
-      const card = await unregisteredIdModel.find({
-        memberId: ctx.params.memberNum,
-      });
-      if (!card) {
-        ctx.throw(404);
-      }
-      ctx.body = card;
-    } catch (err) {
-      if (err.name === 'CastError' || err.name === 'NotFoundError') {
-        ctx.throw(404);
-      }
-      ctx.throw(500);
-    }
-  });
-  //Mark Card Sent
-  router.put('/api/send/:memberId', bodyParser(), async (ctx) => {
-    const update = {
-      sent: true,
-      sentTo: ctx.request.body.sentTo,
-    };
-    try {
-      const updatedId = await unregisteredIdModel.findOneAndUpdate(
-        { memberId: ctx.params.memberId },
-        update
-      );
-      console.log(updatedId);
-      ctx.body = JSON.stringify(updatedId);
-    } catch (err) {
-      console.log(err.name);
-    }
-  });
-
-  // get all members
-  router.get('/api/members', async (ctx) => {
-    ctx.body = await registeredIdModel.find();
-  });
-
-  // get a single membership by email
-  router.get('/api/member-email/:memberEmail', async (ctx) => {
-    try {
-      const memberNum = await registeredIdModel.find({
-        customerEmail: ctx.params.memberEmail,
-      });
-      if (!memberNum) {
-        ctx.throw(404);
-      }
-      ctx.body = memberNum;
-    } catch (err) {
-      if (err.name === 'CastError' || err.name === 'NotFoundError') {
-        ctx.throw(404);
-      }
-      ctx.throw(500);
-    }
-  });
-
-  // get a single membership by account number
-  router.get('/api/member/:memberNum', async (ctx) => {
-    try {
-      const memberNum = await registeredIdModel.find({
-        accountNumber: ctx.params.memberNum,
-      });
-      if (!memberNum) {
-        ctx.throw(404);
-      }
-      ctx.body = memberNum;
-      console.log(memberNum);
-    } catch (err) {
-      if (err.name === 'CastError' || err.name === 'NotFoundError') {
-        ctx.throw(404);
-      }
-      ctx.throw(500);
-    }
-  });
-  //update a single member
-  router.put('/api/update/:memberId', bodyParser(), async (ctx) => {
-    // const idNumber = await registeredIdModel.find({ accountNumber: ctx.params.memberId });
-    const update = {
-      cardTier: ctx.request.body.cardTier,
-      notes: ctx.request.body.notes,
-    };
-    try {
-      const updatedId = await registeredIdModel.findOneAndUpdate(
-        { accountNumber: ctx.params.memberId },
-        update
-      );
-      console.log(updatedId);
-      ctx.body = JSON.stringify(updatedId);
-      shopifyApiCalls.replaceCustomerTags(
-        process.env.SHOPIFY_STORE,
-        ctx.request.body.shopifyCustomerId,
-        ctx.request.body.cardTier
-      );
-    } catch (err) {
-      console.log(err.name);
-    }
-  });
-
-  // register new membership
-  router.post('/api/register/:memberId', bodyParser(), async (ctx) => {
-    const idPrefix = ctx.params.memberId.substring(0, 4);
-    const idNumberMatch = await unregisteredIdModel.find({
-      memberId: ctx.params.memberId,
-    });
-    let tier = '';
-    if (idNumberMatch.length > 0) {
-      try {
-        unregisteredIdModel.findOneAndDelete(
-          { memberId: ctx.params.memberId },
-          function (err, docs) {
-            if (err) {
-              console.log(err);
-            } else {
-              console.log('Deleted Unregistered ID number : ', docs);
-            }
+    server.use(
+      createShopifyAuth({
+        accessMode: 'offline',
+        authPath: '/install/auth',
+        async afterAuth(ctx) {
+          const { shop, accessToken } = ctx.state.shopify;
+          const { host } = ctx.query;
+          if (!accessToken) {
+            // This can happen if the browser interferes with the auth flow
+            ctx.response.status = 500;
+            ctx.response.body = 'Failed to get access token! Please try again.';
+            return;
           }
-        );
-        if (idPrefix === '1658') {
-          tier = 'Red';
-        } else if (idPrefix === '2409') {
-          tier = 'Blue';
-        } else if (idPrefix === '3945') {
-          tier = 'Gold';
-        } else if (idPrefix === '4679') {
-          tier = 'Black';
-        } else {
-          console.error();
-        }
+          // Redirect to user auth endpoint, to get user's online token
+          ctx.redirect(`/auth?shop=${shop}&host=${host}`);
+        },
+      })
+    );
+  });
 
-        let thisUrl = `https://${process.env.SHOPIFY_STORE}.myshopify.com/admin/api/2022-07/customers/${ctx.request.body.shopifyCustomerId}.json`;
+  router.get('(/auth)', verifyRequest(), async (ctx) => {
+    server.use(
+      createShopifyAuth({
+        accessMode: 'online',
+        authPath: '/auth',
+        async afterAuth(ctx) {
+          const { shop } = ctx.state.shopify;
+          const { host } = ctx.query;
+          // Check if the app is installed
+          // NOTE: You can replace with your own function to check if the shop is installed, or you can just remove it, but this is an extra check that can help prevent auth issues
+          if (isShopActive(shop)) {
+            // Redirect to app
+            ctx.redirect(`/?shop=${shop}&host=${host}`);
+          } else {
+            // Redirect to installation endpoint to get permanent access token
+            ctx.redirect(`/install/auth/?shop=${shop}&host=${host}`);
+          }
+        },
+      })
+    );
+  });
 
-        getResponse = await fetch(thisUrl, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-Shopify-Access-Token': process.env.ACCESS_TOKEN,
-          },
-        });
+    router.get('(/_next/static/.*)', verifyRequest);
+    router.get('/_next/webpack-hmr', verifyRequest);
 
-        const getData = await getResponse.json();
-        console.log(getData.customer.tags);
-        const newCustomerTags = getData.customer.tags.concat(`,${tier}`);
-        console.log(newCustomerTags);
+    //get all unregistered cards
+    router.get('/api/unregistered', async (ctx) => {
+      ctx.body = await unregisteredIdModel.find();
+    });
 
-        const requestOptions = {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-Shopify-Access-Token': process.env.ACCESS_TOKEN,
-          },
-          body: JSON.stringify({
-            customer: {
-              id: ctx.request.body.shopifyCustomerId,
-              tags: newCustomerTags,
-            },
-          }),
-        };
-        const response = await fetch(thisUrl, requestOptions);
-        const data = await response.json();
-        console.log(data);
-
-        const newId = await registeredIdModel.create({
-          customerEmail: ctx.request.body.customerEmail,
-          shopifyCustomerId: ctx.request.body.shopifyCustomerId,
-          cardTier: tier,
-          accountNumber: ctx.params.memberId,
-        });
-        console.log(newId);
-        ctx.body = JSON.stringify(newId);
-        google_cal
-          .insertEvent(ctx.request.body.customerEmail)
-          .then((res) => {
-            console.log(res);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      } catch (err) {
-        error.message = 'Invalid account number';
-        console.log(error.message);
-      }
-    } else
+    //get one unregistered card by account number
+    router.get('/api/unregistered/:memberNum', async (ctx) => {
       try {
-        error.message = 'Invalid account number';
+        const card = await unregisteredIdModel.find({
+          memberId: ctx.params.memberNum,
+        });
+        if (!card) {
+          ctx.throw(404);
+        }
+        ctx.body = card;
+      } catch (err) {
+        if (err.name === 'CastError' || err.name === 'NotFoundError') {
+          ctx.throw(404);
+        }
+        ctx.throw(500);
+      }
+    });
+    //Mark Card Sent
+    router.put('/api/send/:memberId', bodyParser(), async (ctx) => {
+      const update = {
+        sent: true,
+        sentTo: ctx.request.body.sentTo,
+      };
+      try {
+        const updatedId = await unregisteredIdModel.findOneAndUpdate(
+          { memberId: ctx.params.memberId },
+          update
+        );
+        console.log(updatedId);
+        ctx.body = JSON.stringify(updatedId);
+      } catch (err) {
+        console.log(err.name);
+      }
+    });
+
+    // get all members
+    router.get('/api/members', async (ctx) => {
+      ctx.body = await registeredIdModel.find();
+    });
+
+    // get a single membership by email
+    router.get('/api/member-email/:memberEmail', async (ctx) => {
+      try {
+        const memberNum = await registeredIdModel.find({
+          customerEmail: ctx.params.memberEmail,
+        });
+        if (!memberNum) {
+          ctx.throw(404);
+        }
+        ctx.body = memberNum;
+      } catch (err) {
+        if (err.name === 'CastError' || err.name === 'NotFoundError') {
+          ctx.throw(404);
+        }
+        ctx.throw(500);
+      }
+    });
+
+    // get a single membership by account number
+    router.get('/api/member/:memberNum', async (ctx) => {
+      try {
+        const memberNum = await registeredIdModel.find({
+          accountNumber: ctx.params.memberNum,
+        });
+        if (!memberNum) {
+          ctx.throw(404);
+        }
+        ctx.body = memberNum;
+        console.log(memberNum);
+      } catch (err) {
+        if (err.name === 'CastError' || err.name === 'NotFoundError') {
+          ctx.throw(404);
+        }
+        ctx.throw(500);
+      }
+    });
+    //update a single member
+    router.put('/api/update/:memberId', bodyParser(), async (ctx) => {
+      // const idNumber = await registeredIdModel.find({ accountNumber: ctx.params.memberId });
+      const update = {
+        cardTier: ctx.request.body.cardTier,
+        notes: ctx.request.body.notes,
+      };
+      try {
+        const updatedId = await registeredIdModel.findOneAndUpdate(
+          { accountNumber: ctx.params.memberId },
+          update
+        );
+        console.log(updatedId);
+        ctx.body = JSON.stringify(updatedId);
+        shopifyApiCalls.replaceCustomerTags(
+          process.env.SHOPIFY_STORE,
+          ctx.request.body.shopifyCustomerId,
+          ctx.request.body.cardTier
+        );
+      } catch (err) {
+        console.log(err.name);
+      }
+    });
+
+    // register new membership
+    router.post('/api/register/:memberId', bodyParser(), async (ctx) => {
+      const idPrefix = ctx.params.memberId.substring(0, 4);
+      const idNumberMatch = await unregisteredIdModel.find({
+        memberId: ctx.params.memberId,
+      });
+      let tier = '';
+      if (idNumberMatch.length > 0) {
+        try {
+          unregisteredIdModel.findOneAndDelete(
+            { memberId: ctx.params.memberId },
+            function (err, docs) {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log('Deleted Unregistered ID number : ', docs);
+              }
+            }
+          );
+          if (idPrefix === '1658') {
+            tier = 'Red';
+          } else if (idPrefix === '2409') {
+            tier = 'Blue';
+          } else if (idPrefix === '3945') {
+            tier = 'Gold';
+          } else if (idPrefix === '4679') {
+            tier = 'Black';
+          } else {
+            console.error();
+          }
+
+          let thisUrl = `https://${process.env.SHOPIFY_STORE}.myshopify.com/admin/api/2022-07/customers/${ctx.request.body.shopifyCustomerId}.json`;
+
+          getResponse = await fetch(thisUrl, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'X-Shopify-Access-Token': process.env.ACCESS_TOKEN,
+            },
+          });
+
+          const getData = await getResponse.json();
+          console.log(getData.customer.tags);
+          const newCustomerTags = getData.customer.tags.concat(`,${tier}`);
+          console.log(newCustomerTags);
+
+          const requestOptions = {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+              'X-Shopify-Access-Token': process.env.ACCESS_TOKEN,
+            },
+            body: JSON.stringify({
+              customer: {
+                id: ctx.request.body.shopifyCustomerId,
+                tags: newCustomerTags,
+              },
+            }),
+          };
+          const response = await fetch(thisUrl, requestOptions);
+          const data = await response.json();
+          console.log(data);
+
+          const newId = await registeredIdModel.create({
+            customerEmail: ctx.request.body.customerEmail,
+            shopifyCustomerId: ctx.request.body.shopifyCustomerId,
+            cardTier: tier,
+            accountNumber: ctx.params.memberId,
+          });
+          console.log(newId);
+          ctx.body = JSON.stringify(newId);
+          google_cal
+            .insertEvent(ctx.request.body.customerEmail)
+            .then((res) => {
+              console.log(res);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        } catch (err) {
+          error.message = 'Invalid account number';
+          console.log(error.message);
+        }
+      } else
+        try {
+          error.message = 'Invalid account number';
+        } catch (err) {
+          console.log(error);
+        }
+    });
+
+    // post a new unregistered Id
+    router.post('/api/memberId', bodyParser(), async (ctx) => {
+      try {
+        const unregisteredId = new unregisteredIdModel(ctx.request.body).save();
+        ctx.body = JSON.stringify(unregisteredId);
       } catch (err) {
         console.log(error);
       }
-  });
-
-  // post a new unregistered Id
-  router.post('/api/memberId', bodyParser(), async (ctx) => {
-    try {
-      const unregisteredId = new unregisteredIdModel(ctx.request.body).save();
-      ctx.body = JSON.stringify(unregisteredId);
-    } catch (err) {
-      console.log(error);
-    }
-  });
-});
+    });
+  
   //keep dynos running
   function keepAwake(url) {
     setInterval(async function () {
