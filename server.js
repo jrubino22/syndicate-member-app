b5c648a8efe4b80e84d48ae46c4ce8e6b7b75315
Joseph Rubino <joeyrubi22@gmail.com>
2023-02-10 16:03:51 -0600

    Fda

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ Shopify.Context.initialize({
   SCOPES: ['write_customers','read_customers'],
   HOST_NAME: process.env.SHOPIFY_APP_URL.replace(/https:\/\//, ''),
   API_VERSION: ApiVersion.June16,
-  IS_EMBEDDED_APP: false,
+  IS_EMBEDDED_APP: true,
   SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
 });
 
@@ -92,9 +92,9 @@ app.prepare().then(() => {
   //installation path
   router.get('/install', async ctx => {
     const shop = ctx.query.shop;
+    const state = CryptoJS.lib.WordArray.random(128/8).toString(CryptoJS.enc.Hex);
     const redirectUri = 'https://syndicate-member.herokuapp.com/auth/callback';
     const installUrl = `https://${shop}.com/admin/oauth/authorize?client_id=${clientId}&scope=write_customers,read_customers&redirect_uri=${redirectUri}&state=${state}`;
-    const state = CryptoJS.lib.WordArray.random(128/8).toString(CryptoJS.enc.Hex);
     console.log("before", ctx.session)
     ctx.session.state = state
     console.log("after", ctx.session)
