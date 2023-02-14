99b854a9d2cbe096d8d86dbd10c0ef12eaf9bde9
Joseph Rubino <joeyrubi22@gmail.com>
2023-02-14 10:43:38 -0600

    fda

--- server.js
+++ server.js
@@ -38,12 +38,14 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = new Koa();
 
+  if (!mongoose.models.Session) {
   const sessionSchema = new mongoose.Schema({
     state: {type: String},
     accessToken: {type: String},
     expires: { type: Date, default: Date.now, expires: '1d' }
   });
   const Session = mongoose.model('Session', sessionSchema, 'sessions');
+}
 
   server.keys = ["fdsgshse, fasdgre"];
   server.use(
