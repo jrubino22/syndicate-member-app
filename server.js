91f5666c558edb49cde80d2d501951567894a186
Joseph Rubino <joeyrubi22@gmail.com>
2023-02-14 12:04:02 -0600

    fda

--- server.js
+++ server.js
@@ -45,7 +45,11 @@ app.prepare().then(() => {
       store: new MongooseStore({
         collection: 'sessions',
         expires: 864000
-      })
+      }),
+      cookie: {
+        sameSite: 'none',
+        secure: true
+      }
     }, server));
     
 
@@ -70,17 +74,17 @@ app.prepare().then(() => {
     ctx.res.statusCode = 200;
   };
 
-  // server.use(cors({
-  //   origin: function (origin, callback) {
-  //     // if (!origin) return callback(null, true);
-  //     if (allowedOrigins.indexOf(origin) === -1) {
-  //       var msg = 'The CORS policy for this site does not ' +
-  //                 'allow access from the specified Origin.';
-  //       return callback(new Error(msg), false);
-  //     }
-  //     return callback(null, true);
-  //   }
-  // }));
+  server.use(cors({
+    origin: function (origin, callback) {
+      // if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  }));
   
 
   router.get('(/_next/static/.*)', handleRequest);
